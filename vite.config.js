89e649c249ac19036002230ca6104a89794ea9f1
Sicknostalgia <89644728+Sicknostalgia@users.noreply.github.com>
2025-10-18 06:25:23 -0700

    WORKING CHECKPOINT

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,5 @@ export default defineConfig({
   plugins: [react()],
   // // tells Vite to generate relative URLs in the build. This is particularly useful when deploying the app to a subdirectory or when the exact deployment path is not known in advance.
   base: './',
+  //finally working checkpoint
 })
