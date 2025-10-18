fadaa618b58cc49a82a40f7c62cb261f05ed3fe3
Sicknostalgia <89644728+Sicknostalgia@users.noreply.github.com>
2025-10-18 03:51:33 -0700

    fixing page not showing realtime
    local host work, custom domain not

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // tells Vite to generate relative URLs in the build. This is particularly useful when deploying the app to a subdirectory or when the exact deployment path is not known in advance.
+  base: './',
 })
