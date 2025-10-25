00bb1140974a526b03a34c5f48abafa066146dc9
Sicknostalgia <89644728+Sicknostalgia@users.noreply.github.com>
2025-10-25 06:55:17 -0700

    add identfiier projects
    replace work to projects

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -25,7 +25,7 @@ const WorkExperience = () => {
               <Suspense 
               fallback={<CanvasLoader />}
               >
-                <Developer position-y={-3} scale={1} animationName={animationName} />
+                <Developer position-y={-3} scale={3} animationName={animationName} />
               </Suspense>
             </Canvas>
           </div>
